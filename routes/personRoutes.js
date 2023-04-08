640e3f8269824696a73292e4b385bad0734af68e
Ruan Silva <ruansilva6721@gmail.com>
2023-04-08 13:31:54 -0300

    add update of person

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,47 +1,80 @@
-const router = require('express').Router()
-const Person = require('../models/Person')
+const router = require("express").Router();
+const Person = require("../models/Person");
 
-router.post('/', async (req, res) =>{
-    //req.body
-    const {name, salary, approved} = req.body
+router.post("/", async (req, res) => {
+  //req.body
+  const { name, salary, approved } = req.body;
 
-    const person = {
-        name,
-        salary,
-        approved
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+  if (!name) {
+    res.status(422).json({ error: "O nome é obrigatório!" });
+    return;
+  }
+
+  try {
+    await Person.create(person);
+
+    res.status(201).json({ message: "Pessoa criada com sucesso!!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+router.get("/index", (req, res) => {
+  res.json({
+    message: "oi Express!",
+  });
+  // console.log(process.env.USER_MONGODB, `${process.env.USER_PASSWORD}`)
+});
+router.get("/", async (req, res) => {
+  try {
+    const people = await Person.find();
+
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const person = await Person.findOne({ _id: id });
+
+    if (!person) {
+      res.status(422).json({ error: "Usuário não encontrado!!" });
+      return;
     }
-    if(!name){
-      res.status(422).json({error: "O nome é obrigatório!"})
+
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+router.patch("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, salary, approved } = req.body;
+
+    const personUpdate = {
+      name,
+      salary,
+      approved,
+    };
+    const person = await Person.updateOne({ _id: id }, personUpdate);
+
+    if (!person) {
+      res.status(422).json({ error: "Usuário não encontrado!!" });
+      return;
     }
 
-    try {
+    res.status(200).json(personUpdate);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
-      await Person.create(person)
-
-      res.status(201).json({message:"Pessoa criada com sucesso!!"})
-      
-    } catch (error) {
-      res.status(500).json({error: error})
-      
-    }
-
-})
-router.get('/index', (req, res) => {
-
-    res.json({
-        message:"oi Express!"
-    })
-   // console.log(process.env.USER_MONGODB, `${process.env.USER_PASSWORD}`)
-})
-router.get('/', async (req, res) =>{
-    try {
-        const people = await Person.find()
-
-        res.status(200).json(people)
-        
-    } catch (error) {
-        res.status(500).json({error: error})  
-    }
-})
-
-module.exports = router
+module.exports = router;
