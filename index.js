53c0efeafb9b4b707af66c786886c6473b1f3f9c
Ruan Silva <ruansilva6721@gmail.com>
2023-04-08 00:35:07 -0300

    first route

--- index.js
+++ index.js
@@ -3,10 +3,22 @@
 const express =  require('express');
 const app = express()
 
-//forma de ler JSON
+//forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
 
+app.use(express.json())
 
 //rota inicial / endpoint
+app.get('/', (req, res) => {
 
+    res.json({
+        message:"oi Express!"
+    })
+})
 
 //entregar um porta
+app.listen(3000)
