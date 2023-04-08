0d0e73665f248b5bc09c5dcd36a66e0215804ed8
Ruan Silva <ruansilva6721@gmail.com>
2023-04-08 01:58:15 -0300

    connecting with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // configuração inicial
-
+require('dotenv').config()
 const express =  require('express');
+const mongoose = require('mongoose')
 const app = express()
 
 //forma de ler JSON / middlewares
@@ -18,7 +19,18 @@ app.get('/', (req, res) => {
     res.json({
         message:"oi Express!"
     })
+   // console.log(process.env.USER_MONGODB, `${process.env.USER_PASSWORD}`)
 })
-
 //entregar um porta
-app.listen(3000)
+
+//connect db
+const DB_USER = encodeURIComponent(process.env.USER_MONGODB)
+const DB_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD)
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.vw9t8la.mongodb.net/?retryWrites=true&w=majority`)
+  .then(()  => {
+    console.log('Conectando ao Mongo!!')
+    app.listen(3000, () => {
+      console.log('Servidor Express rodando na porta 3000!')
+    })
+  })
+  .catch((error) => console.log(error))
